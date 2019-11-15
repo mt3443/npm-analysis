6a64a8b92d1748aaa83e91d973049a94d6bbc63a
mt3443 <mt3443@gmail.com>
2019-11-15 14:49:15 -0600

    Finished dynamic cluster script generator

--- generate_job_script.py
+++ generate_job_script.py
@@ -1,6 +1,7 @@
 import os
 import re
 
+print('Getting node info...', flush=True)
 os.system('scontrol show node > node_info')
 node_info = open('node_info', 'r').readlines()
 
@@ -35,8 +36,9 @@ for node in unused_nodes:
 	total_cores += int(n_cores)
 
 # get all package names
+print('Getting package names...', flush=True)
 packages_dir = '/volatile/m139t745/npm-packages'
-all_packages = [f for f in os.listdir(packages_dir) if os.path.isdir(os.path.join(packages_dir, f))]
+all_packages = [f for f in os.listdir(packages_dir)]
 total_packages = len(all_packages)
 
 # divide packages equally among number of cores
@@ -44,16 +46,39 @@ packages_per_core = int(total_packages / total_cores) + 1
 
 # prep package_names directory
 # assign packages to cores
+print('Assigning packages to cores...', flush=True)
 os.system('rm -rf package_names')
 os.mkdir('package_names')
 
 for node in nodes_cores:
 	f = open('package_names/{}'.format(node), 'w')
-	for p in range(nodes_cores[node] * packages_per_core):
+	for p in range(int(nodes_cores[node]) * packages_per_core):
 		f.write('{}\n'.format(all_packages.pop()))
+		if len(all_packages) == 0:
+			break
 	f.close()
 
-for node in nodes_cores:
-	os.system('srun -N 1 -n {} -c 1 -w {} --exclusive python3 test_model.py {} {} &'.format(nodes_cores[node], node, node, nodes_cores[node]))
+# write batch_job.sh file
+print('Generating job.sh...', flush=True)
+f = open('job.sh', 'w')
 
-os.system('wait')
+f.write('#!/bin/bash\n')
+f.write('\n')
+f.write('#SBATCH -N {}\n'.format(len(nodes_cores)))
+f.write('#SBATCH -n {}\n'.format(total_cores))
+f.write('#SBATCH -c 1\n')
+f.write('#SBATCH -t 48:00:00\n')
+f.write('#SBATCH --mem-per-cpu=4096\n')
+f.write('#SBATCH -J npm-analysis\n')
+f.write('#SBATCH -o slurm-%j.out\n')
+f.write('\n')
+
+for node in nodes_cores:
+	f.write('srun -N 1 -n {} -c 1 -w {} --exclusive python3 test_model.py {} {} &\n'.format(nodes_cores[node], node, node, nodes_cores[node]))
+
+f.write('wait\n')
+
+f.close()
+
+print('Done')
+print('Enter: \'sbatch job.sh\' to begin job')
