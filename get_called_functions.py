b1d65244bd7112c228a2f4c1f045c9909a0dd011
mt3443 <mt3443@gmail.com>
2019-11-19 21:48:51 -0600

    Suppressed esprima exceptions when given malformed javascript files

--- get_called_functions.py
+++ get_called_functions.py
@@ -1,11 +1,16 @@
-import subprocess
+from subprocess import check_output, CalledProcessError
 
 def get_called_functions(js_files):
     all_functions = set()
 
     for f in js_files:
-        result = subprocess.check_output('node find_functions.js {}'.format(f[:-1]), shell=True)
-        functions = set(result.decode('utf8').split())
-        all_functions = all_functions.union(functions)
+        try:
+            result = check_output('node find_functions.js {}'.format(f[:-1]))
+
+            functions = set(result.decode('utf8').split())
+            all_functions = all_functions.union(functions)
+        
+        except CalledProcessError as e:
+            continue
 
     return list(all_functions)
