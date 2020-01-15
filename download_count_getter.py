c08c0be2248c28eebf9522b24f602237bb5c8f29
mt3443 <mt3443@gmail.com>
2020-01-15 16:20:19 -0600

    Download getter fix

--- download_count_getter.py
+++ download_count_getter.py
@@ -1,12 +1,13 @@
 import requests
 import time
 import sys
+import os
 
 downloads_endpoint = 'https://api.npmjs.org/downloads/point/last-week'
-all_packages = [x[:-1] for x in open('download_package_names/{}'.format(sys.argv[1])).readlines()]
+all_packages = [x[:-1] for x in open('/users/m139t745/npm-analysis/download_package_names/{}'.format(sys.argv[1])).readlines()]
 
-success = open('data/download_counts/success/{}'.format(sys.argv[1]), 'w')
-fail = open('data/download_counts/fail/{}'.format(sys.argv[1]), 'w')
+success = open('/dev/shm/success', 'w')
+fail = open('/dev/shm/fail', 'w')
 
 for package_name in all_packages:
     try:
@@ -19,9 +20,15 @@ for package_name in all_packages:
         
         if r.status_code != 200:
             fail.write('{} returned {}'.format(package_name, r.status_code))
+            fail.flush()
             continue
 
         success.write('{},{}'.format(package_name, r.json()['downloads']))
+        success.flush()
 
     except Exception as e:
         fail.write('{} error'.format(package_name))
+        fail.flush()
+
+os.system('mv /dev/shm/success /users/m139t745/npm-analysis/data/download_counts/success/{}'.format(sys.argv[1]))
+os.system('mv /dev/shm/fail /users/m139t745/npm-analysis/data/download_counts/fail/{}'.format(sys.argv[1]))
