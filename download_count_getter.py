98a44aab473cd7eab40c9b70f38ddb082d094776
mt3443 <mt3443@gmail.com>
2020-01-15 16:22:52 -0600

    Added log file new lines

--- download_count_getter.py
+++ download_count_getter.py
@@ -16,18 +16,18 @@ for package_name in all_packages:
         if r.status_code == 429:
             while r.status_code == 429:
                 time.sleep(30)
-                r = requests.get('{}/{}'.format(downloads_endpoint, package_name))
+                r = requests.get('{}/{}\n'.format(downloads_endpoint, package_name))
         
         if r.status_code != 200:
-            fail.write('{} returned {}'.format(package_name, r.status_code))
+            fail.write('{} returned {}\n'.format(package_name, r.status_code))
             fail.flush()
             continue
 
-        success.write('{},{}'.format(package_name, r.json()['downloads']))
+        success.write('{},{}\n'.format(package_name, r.json()['downloads']))
         success.flush()
 
     except Exception as e:
-        fail.write('{} error'.format(package_name))
+        fail.write('{} error\n'.format(package_name))
         fail.flush()
 
 os.system('mv /dev/shm/success /users/m139t745/npm-analysis/data/download_counts/success/{}'.format(sys.argv[1]))
