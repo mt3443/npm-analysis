3611b8d45e9a8970410546a4209c0b672b019deb
mt3443 <mt3443@gmail.com>
2020-01-16 18:21:11 -0600

    Fixed incorrect path

--- typosquatting/start_transitive.py
+++ typosquatting/start_transitive.py
@@ -5,7 +5,7 @@ node = sys.argv[1]
 os.system('rm -rf /dev/shm/npm')
 os.system('mkdir -p /dev/shm/npm/transitive')
 os.system('mkdir /dev/shm/npm/data')
-os.system('cp run_transitive.js /dev/shm/npm/typosquatting')
+os.system('cp run_transitive.js /dev/shm/npm/transitive')
 os.system('cp ../data/typosquatting_candidates.txt /dev/shm/npm/data')
 os.system('node /dev/shm/npm/transitive/run_transitive.js {}'.format(node))
 os.system('mv /dev/shm/npm/transitive/positive /users/m139t745/npm-analysis/typosquatting/transitive_output/positive/{}'.format(node))
