64ad655a1291295169d2503c6e58127dc83c6525
mt3443 <mt3443@gmail.com>
2020-01-21 15:37:58 -0600

    Fixed removal of processed positives

--- typosquatting/cluster.py
+++ typosquatting/cluster.py
@@ -10,17 +10,20 @@ random.shuffle(all_packages)
 del all_packages_json
 
 # remove package names that have already been analyzed
-if os.path.exists('output2'):
+if os.path.exists('output1'):
     all_packages_set = set(all_packages)
-    log_dirs = ['output2/negative', 'output2/positive']
+    log_dirs = ['output1/negative', 'output1/positive', 'output2/negative', 'output2/positive', 'output3/negative', 'output3/positive']
     
     for d in log_dirs:
         log_files = os.listdir(d)
         for l in log_files:
             packages = open(os.path.join(d, l)).read().splitlines()
             for p in packages:
-                if p in all_packages_set:
-                    all_packages_set.remove(p)
+                if len(p.split(',')) > 4:
+                    continue
+                x = p.split(',')[0]
+                if x in all_packages_set:
+                    all_packages_set.remove(x)
     
     all_packages = list(all_packages_set)
     del all_packages_set
